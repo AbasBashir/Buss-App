0a0b186747a03b52a124715cc7b0c7ec23297f40
AbasBashir <MrAbasBashir@gmail.com>
2023-10-04 14:08:41 +0200

    first commit for js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// First we need a variable to store counting, we declare by setting it 0 first.
+
+//Next, ww need to "listen" for clicks and when the button is clicked, we increment the button variable 
+
+//Finally we need to change the content inside id attribute count-people to reflect the new value
+
+function increment(){
+    
+    console.log("HELOOOOOO");
+}
